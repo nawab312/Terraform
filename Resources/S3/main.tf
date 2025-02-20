e1164c5caf089e67e0d7b6b4bd4ead82df425e7a
Siddharth Singh <72911921+nawab312@users.noreply.github.com>
2025-02-20 15:51:23 +0530

    Create main.tf

---
+++ Resources/S3/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+    bucket = "my-sid-bucket-3121" #Must be Globally Unique
+    tags = {
+      Name = "MyS3Bucker"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {  
+    bucket = aws_s3_bucket.my_bucket.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
